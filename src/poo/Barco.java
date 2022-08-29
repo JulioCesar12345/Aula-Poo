c473a07126511ce7b5fabfb2434e23667c22a169
Julio Cesar Dellani Gabardo <julio.gabardo@alunos.sc.senac.br>
2022-08-29 20:22:22 -0300

    Atividade POO

---
+++ src/poo/Barco.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author julio.gabardo
+ */
+public class Barco {
+    
+    String cor;
+    double tamanho;
+    String nome;
+    String tipo;
+    int tripulacao;
+    
+    public String getNome(){
+        return nome;     
+    }
+    public void setNome(String nome){
+        this.nome= nome;
+    }
+    public double getTamanho(){
+        return tamanho;     
+    }
+    public void setTamanho(double tamanho){
+        this.tamanho= tamanho;
+    }
+    public String getCor(){
+        return cor;     
+    }
+    public void setCor(String cor){
+        this.cor= cor;
+    }
+    public String getTipo(){
+        return tipo;     
+    }
+    public void setTipo(String tipo){
+        this.tipo= tipo;
+    }
+    public int getTripulacao(){
+        return tripulacao;     
+    }
+    public void setTripulacao(int tripulacao){
+        this.tripulacao= tripulacao;
+    }
+    
+}
---
+++ src/poo/Carro.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author julio.gabardo
+ */
+public class Carro {
+
+    String tipo;
+    String cor;
+    String placa;
+    int numPortas;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getNumPortas() {
+        return numPortas;
+    }
+
+    public void setNumPortas() {
+        this.numPortas = numPortas;
+    }
+
+}
